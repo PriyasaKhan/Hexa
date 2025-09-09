55180e8416190c4c9cdefd647410de1f03590412
PriyasaKhan <mampi40014448@gmail.com>
2025-09-09 13:41:38 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,456 @@
+let radio = document.querySelector('#check');
+
+let offf = document.querySelector('#offf');
+let onnn = document.querySelector('#onnn');
+let body = document.querySelector('body');
+let nav = document.querySelector('nav');
+let logo = document.querySelector('.logo p')
+let btn1 = document.querySelector('.btn1')
+let home = document.querySelector('.home');
+let portfolio = document.querySelector('.portfolio');
+let cr = document.querySelector('.cr');
+let news = document.querySelector('.news');
+let contact = document.querySelector('.contact');
+let landingpage = document.querySelector('.landing-page')
+let texth1 = document.querySelector('.text h1')
+let blur1 = document.querySelector('.blur1');
+let blur2 = document.querySelector('.blur2');
+let span = document.querySelector('.span')
+let ltext1 = document.querySelector('.logo-text1')
+let cir = document.querySelector('.cir');
+let cirp = document.querySelector('.cir p');
+let project = document.querySelector('.project');
+let marq = document.querySelector('.mar-q');
+let txt1 = document.querySelector('.txt1')
+let ltimg = document.querySelector('.logo-text img');
+let img1 = document.querySelector('.img1');
+let img02 = document.querySelector('.img-2');
+let img3 = document.querySelector('.img3');
+let img4 = document.querySelector('.img4');
+let img5 = document.querySelector('.img5');
+let img6 = document.querySelector('.img6');
+let services = document.querySelector('.services');
+let cir1 = document.querySelector('.cir1');
+let cir2p = document.querySelector('.cir2 p');
+let sech1 = document.querySelector('.sec-h1');
+let about = document.querySelector('.about');
+let blur3 = document.querySelector('.blur3')
+let linep = document.querySelector('.line-p')
+let line = document.querySelector('.line');
+let user = document.querySelector('#user');
+let g = document.querySelector('#g');
+let text1h3 = document.querySelector('.text1 h3');
+let text1p = document.querySelector('.text1 p');
+let ataa = document.querySelector('#ataa');
+let countries = document.querySelector('#countries');
+let cir2 = document.querySelector('#cir2');
+let cir3p = document.querySelector('#cir3 p');
+let create = document.querySelector('#create');
+let  f1 = document.querySelector('.f1');
+let  f2 = document.querySelector('.f2');
+let  f3 = document.querySelector('.f3');
+let last = document.querySelector('.last');
+let mq2 = document.querySelector('.mq2');
+let client = document.querySelector('.client');
+let see1p = document.querySelector('.see1 p');
+let cr1 = document.querySelector('#cr1');
+let cr2 = document.querySelector('#cr2');
+let cr3 = document.querySelector('#cr3');
+let q1 = document.querySelector('.q1');
+let q2 = document.querySelector('.q2');
+let q3 = document.querySelector('.q3');
+let rp = document.querySelector('#rp');
+let zara = document.querySelector('.zara');
+let fd = document.querySelector('.fd');
+let sara= document.querySelector('.sara');
+let tara= document.querySelector('.tara');
+let rp2 = document.querySelector('#rp2');
+let rp3 = document.querySelector('#rp3');
+let professional = document.querySelector('.professional');
+let faces1 = document.querySelector('.faces1');
+let faces2 = document.querySelector('.faces2');
+let faces3 = document.querySelector('.faces3');
+let faces4 = document.querySelector('.faces4');
+let blur6 = document.querySelector('.blur6');
+let psh = document.querySelector('#psh');
+let csh = document.querySelector('#csh');
+let clicp = document.querySelector('.clicp');
+let clic1 = document.querySelector('#clic1');
+let procp = document.querySelector('.procp');
+let proc1 = document.querySelector('#proc1');
+let port = document.querySelector('.port')
+let portp = document.querySelector('.portp');
+let portc1 = document.querySelector('#portc1');
+let porth2= document.querySelector('#porth2');
+let blur5 = document.querySelector('.blur5');
+let see = document.querySelector('.see');
+let arrow = document.querySelector('#arrow');
+let firstbox = document.querySelector('.first-box');
+let pic1 = document.querySelector('.pic1');
+let arr1 = document.querySelector('.arr1');
+let icons = document.querySelector('.icons');
+
+
+
+
+
+
+
+radio.addEventListener('click', () => {
+    if (radio.checked == false) {
+
+        logo.style.color = '#0095ffff'
+     landingpage.style.backgroundColor='#f8f8f8ff';
+      blur1.style.backgroundColor = '#0095ffff';
+        blur2.style.backgroundColor = '#0095ffff';
+ btn1.style.backgroundColor = 'transparent'
+    btn1.style.border = '2px solid #0095ffff'
+    btn1.style.color = 'black';
+    btn1.style.color = '#0095ffff';
+home.style.color = '#0095ffff';
+    portfolio.style.color = 'black';
+    cr.style.color = 'black';
+    news.style.color = 'black';
+    contact.style.color = 'black';
+
+home.addEventListener('mouseenter',()=>{
+    home.style.color='#0095ffff'
+})
+portfolio.addEventListener('mouseenter',()=>{
+    portfolio.style.color='#0095ffff'
+})
+cr.addEventListener('mouseenter',()=>{
+    cr.style.color='#0095ffff'
+})
+news.addEventListener('mouseenter',()=>{
+    news.style.color='#0095ffff'
+})
+contact.addEventListener('mouseenter',()=>{
+    contact.style.color='#0095ffff'
+})
+
+home.addEventListener('mouseleave',()=>{
+    home.style.color='#0095ffff'
+})
+portfolio.addEventListener('mouseleave',()=>{
+    portfolio.style.color='black'
+})
+cr.addEventListener('mouseleave',()=>{
+    cr.style.color='black'
+})
+news.addEventListener('mouseleave',()=>{
+    news.style.color='black'
+})
+contact.addEventListener('mouseleave',()=>{
+    contact.style.color='black'
+})
+texth1.style.color = '#0095ffff';
+
+    span.style.color = '#0095ffff';
+    ltext1.style.color = '#0095ffff';
+    cir.style.backgroundColor = '#75c2faff';
+    cirp.style.color = 'white';
+    
+    cir.addEventListener('mouseenter',()=>{
+    cir.style.backgroundColor='#75c2faff'
+})
+cir.addEventListener('mouseleave',()=>{
+    cir.style.backgroundColor='#75c2faff';
+})
+    project.style.backgroundColor = '#f8f8f8ff';
+    marq.style.backgroundColor = '#f8f8f8ff';
+    txt1.style.color = '#0095ffff';
+ltimg.src = 'img/dm-hero2.png';
+    img1.src = 'img/dm-brand012.png';
+    img02.src = 'img/dm-brand022.png';
+    img3.src = 'img/dm-brand033.png';
+    img4.src = 'img/dm-brand044.png';
+    img5.src = 'img/dm-brand055.png';
+    img6.src = 'img/dm-brand066.png';
+services.style.backgroundColor = '#f8f8f8ff';
+cir1.style.backgroundColor= '#0095ffff';
+cir2p.style.color = 'gray'
+sech1.style.color='#0095ffff'
+about.style.backgroundColor = '#f8f8f8ff';
+blur3.style.backgroundColor = '#0095ffff';
+linep.style.color='gray';
+line.style.border = '1px solid #0095ffff';
+user.style.color = '#0095ffff';
+g.style.color = '#0095ffff';
+text1h3.style.color= '#0095ffff';
+text1p.style.color= 'gray';
+ataa.style.color = '#0095ffff';
+countries.style.color = 'gray';
+cir3p.style.color = 'gray';
+cir2.style.backgroundColor = '#0095ffff';
+create.style.color = '#0095ffff';
+f1.style.color = '#0095ffff';
+f2.style.color = '#9e9e9e';
+f3.style.color = '#0095ffff';
+last.style.backgroundColor='#f8f8f8ff';
+mq2.style.backgroundColor='#f8f8f8ff';
+client.style.backgroundColor='#f8f8f8ff'; 
+see1p.style.color='gray';
+cr1.style.backgroundColor='#0095ffff';
+cr2.style.backgroundColor='#0095ffff';
+cr3.style.backgroundColor='#0095ffff';
+q1.style.color='white';
+q2.style.color='white';
+q3.style.color='white';
+rp.style.color='rgba(106, 106, 106, 1)';
+rp2.style.color='rgba(106, 106, 106, 1)';
+rp3.style.color='rgba(106, 106, 106, 1)';
+zara.style.color='#0095ffff';
+sara.style.color='#0095ffff';
+tara.style.color='#0095ffff';
+fd.style.color='rgb(105, 105, 105)';
+professional.style.backgroundColor = '#f8f8f8ff';
+ blur6.style.backgroundColor = '#0095ffff';
+psh.style.color = '#0095ffff';
+csh.style.color = '#0095ffff';
+clicp.style.color='gray';
+clic1.style.backgroundColor='#0095ffff';
+procp.style.color='gray';
+proc1.style.backgroundColor='#0095ffff';
+port.style.backgroundColor='#f8f8f8ff';
+portp .style.color='gray';
+portc1.style.backgroundColor='#0095ffff'
+porth2.style.color='#0095ffff';
+blur5.style.backgroundColor = '#0095ffff';
+see.style.color='#0095ffff';
+arrow.style.color='white';
+icons.addEventListener('mouseenter',()=>{
+icons.style.backgroundColor='linear-gradient(177deg,rgba(255, 255, 255, 1) 27%, #0095ffff 100%)';
+
+})
+
+icons.addEventListener('mouseleave',()=>{
+icons.style.backgroundColor='transparent';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    else {
+
+        logo.style.color = '#c4ef17';
+         blur1.style.backgroundColor = '#c4ef17';
+        blur2.style.backgroundColor = '#c4ef17';
+       landingpage.style.backgroundColor='black'
+ btn1.style.border = '2px solid #c4ef17';
+    btn1.style.backgroundColor = 'transparent';
+    btn1.style.color = '#c4ef17';
+   home.style.color = '#c4ef17'
+    portfolio.style.color = 'gainsboro';
+    cr.style.color = 'gainsboro';
+    news.style.color = 'gainsboro';
+    contact.style.color = 'gainsboro';
+
+home.addEventListener('mouseenter',()=>{
+    home.style.color='#c4ef17'
+})
+portfolio.addEventListener('mouseenter',()=>{
+    portfolio.style.color='#c4ef17'
+})
+cr.addEventListener('mouseenter',()=>{
+    cr.style.color='#c4ef17'
+})
+news.addEventListener('mouseenter',()=>{
+    news.style.color='#c4ef17'
+})
+contact.addEventListener('mouseenter',()=>{
+    contact.style.color='#c4ef17'
+})
+
+home.addEventListener('mouseleave',()=>{
+    home.style.color='#c4ef17'
+})
+portfolio.addEventListener('mouseleave',()=>{
+    portfolio.style.color='gainsboro'
+})
+cr.addEventListener('mouseleave',()=>{
+    cr.style.color='gainsboro'
+})
+news.addEventListener('mouseleave',()=>{
+    news.style.color='gainsboro'
+})
+contact.addEventListener('mouseleave',()=>{
+    contact.style.color='gainsboro'
+})
+ texth1.style.color = 'white'
+    span.style.color = '#c4ef17'
+    ltext1.style.color = 'white'
+    cir.style.backgroundColor = 'white';
+
+    cir.addEventListener('mouseenter',()=>{
+    cir.style.backgroundColor='#c4ef17'
+})
+cir.addEventListener('mouseleave',()=>{
+    cir.style.backgroundColor='white';
+})
+    cirp.style.color = 'black';
+    project.style.backgroundColor = 'black';
+    marq.style.backgroundColor = 'black';
+    txt1.style.color = 'gainsboro';
+
+ltimg.src = 'img/dm-hero.png';
+    img1.src = 'img/dm-brand01.png';
+    img02.src = 'img/dm-brand02.png';
+    img3.src = 'img/dm-brand03.png';
+    img4.src = 'img/dm-brand04.png';
+    img5.src = 'img/dm-brand05.png';
+    img6.src = 'img/dm-brand06.png';
+services.style.backgroundColor = 'black';
+cir1.style.backgroundColor= '#c4ef17';
+cir2p.style.color = 'white';
+sech1.style.color='white';
+about.style.backgroundColor = 'black';
+blur3.style.backgroundColor = '#c4ef17';
+linep.style.color='white';
+line.style.border = '1px solid gray';
+user.style.color = 'white';
+g.style.color = 'white';
+text1h3.style.color= 'white';
+text1p.style.color= 'white';
+ataa.style.color = 'white';
+countries.style.color = 'white';
+cir3p.style.color = 'white';
+cir2.style.backgroundColor = '#c4ef17';
+create.style.color = 'white';
+f1.style.color = 'white';
+f2.style.color = 'white';
+f3.style.color = 'white';
+last.style.backgroundColor='black';
+mq2.style.backgroundColor='black';
+client.style.backgroundColor='black'; 
+see1p.style.color='rgb(214 209 209)';
+
+
+cr1.style.backgroundColor='#c4ef17';
+cr2.style.backgroundColor='#c4ef17';
+cr3.style.backgroundColor='#c4ef17';
+q1.style.color='black';
+q2.style.color='black';
+q3.style.color='black';
+rp.style.color='rgb(189, 189, 189)';
+rp2.style.color='rgb(189, 189, 189)';
+rp3.style.color='rgb(189, 189, 189)';
+zara.style.color='white';
+sara.style.color='white';
+tara.style.color='white';
+fd.style.color='rgb(105, 105, 105)';
+professional.style.backgroundColor = 'black';
+   blur6.style.backgroundColor = '#c4ef17';
+psh.style.color = 'white';
+csh.style.color = 'white';
+clicp.style.color='white';
+clic1.style.backgroundColor='#c4ef17';
+procp.style.color='white';
+proc1.style.backgroundColor='#c4ef17';
+port.style.backgroundColor='black';
+portp .style.color='white';
+portc1.style.backgroundColor='#c4ef17'
+porth2.style.color='white';
+blur5.style.backgroundColor = '#c4ef17';
+see.style.color='white';
+arrow.style.color='white';
+
+icons.addEventListener('mouseenter',()=>{
+icons.style.backgroundColor='linear-gradient(177deg,rgba(255, 255, 255, 1) 27%, rgba(196, 239, 23, 1) 100%)';
+icons.style.display ='flex'
+})
+
+icons.addEventListener('mouseleave',()=>{
+icons.style.backgroundColor='transparent';
+icons.style.display ='none'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   firstbox.addEventListener('mouseenter',()=>{
+    firstbox.style.backgroundColor='linear-gradient(177deg,rgba(10, 10, 10, 0.475) 40%, rgba(196, 239, 23, 1) 100%)'
+    pic1.style.filter='grayscale(0) opacity(1)';
+    arr1.style.transform='rotate(5deg)';
+});
+firstbox.addEventListener('mouseleave',()=>{
+    firstbox.style.backgroundColor='rgba(0, 0, 0, 0.175)'
+    pic1.style.filter='grayscale(0.9) opacity(0.5)';
+    arr1.style.transform='rotate(-25deg)';
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
